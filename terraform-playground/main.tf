eb04df67a1e077b61231c9f204243a18061b4524
gjwoo960101 <158238808+gjwoo960101@users.noreply.github.com>
2025-05-02 07:25:28 +0000

    local 연습중

---
+++ terraform-playground/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    random = {
+        source = "hashicorp/random"
+    }
+    local = {
+        source = "hashicorp/local"
+    }
+    null = {
+        source = "hashicorp/null"
+    }
+  }
+}
+
+provider "random" {
+  
+}
+
+provider "local" {
+  
+}
+
+provider "null" {
+  
+}
+
+resource "random_pet" "pet_name" {
+  length = 2
+}
+
+resource "local_file" "write_pet_name" {
+  content = "Your random pet name is: ${random_pet.pet_name.id}"
+  filename = "${path.module}/pet_name.txt"
+}
+
+resource "null_resource" "log_output" {
+  provisioner "local-exec" {
+    command = "echo 'Your random pet name is: ${random_pet.pet_name.id}'"
+  }
+  # pet name이 바뀔 때마다 실행되도록 트리거 설정
+  triggers = {
+    pet_name = random_pet.pet_name.id
+  }
+}
